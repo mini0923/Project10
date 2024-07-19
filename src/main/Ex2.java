9fb2604cfab1955b27acf31b9e55c44ea2122fba
G202 <G202@DESKTOP-NAPAO38>
2024-07-19 10:37:42 +0900

    hhhh

--- src/main/Ex2.java
+++ src/main/Ex2.java
@@ -7,9 +7,11 @@ public class Ex2 {
 		// 인터페이스 형변환
 		// 부모타입 변수 선언 = 자식 타입 객체 생성
 		
-		Calc calc = new Calculator();
+	    Calc calc = new Calculator();
 		
 		calc.add(2,3);
+		
+		//aa
 		
 		// 형변환된 변수로는 자식클래스에서 추가한 속성을 사용할 수 없음!
 		
